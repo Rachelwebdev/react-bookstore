2326e5bed4935eb3eacc568c3d4d3dc65d536ef3
Rachel Isaac <rachelisaac995@gmail.com>
2023-02-23 18:39:34 +0400

    Create routes that link to different pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import './App.css';
+import './styles/App.module.css';
+import { Routes, Route } from 'react-router-dom';
+import Books from './components/Books';
+import Categories from './components/Categories';
+import Navbar from './components/Navbar';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <h1>HOME PAGE</h1>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={<Books />}
+        />
+        <Route
+          path="/categories"
+          element={<Categories />}
+        />
+        <Route
+          path="*"
+          element={<NotFound />}
+        />
+      </Routes>
+    </>
   );
 }
 
