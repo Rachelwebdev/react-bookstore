d4db066fa3157583b3e3de1dddca044381ea212d
Rachel Isaac <rachelisaac995@gmail.com>
2023-02-23 18:40:24 +0400

    Add Books Component

---
+++ src/components/Books.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Book from './Book';
+
+function Books() {
+  return (
+    <>
+      <ul>
+        <Book />
+        <Book />
+        <Book />
+        <Book />
+      </ul>
+    </>
+  );
+}
+
+export default Books;
