8f589536908d13fdfad4af6b6c913d2cb7fbcf90
Rachel Isaac <rachelisaac995@gmail.com>
2023-02-28 19:05:18 +0400

    Add categories slice

---
+++ src/redux/categories/categoriesSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  categories: [],
+};
+
+const categoriesSlice = createSlice({
+  name: 'categories',
+  initialState,
+  reducers: {
+    checkStatus: (state) => {
+      if (state.categories.length === 0) {
+        return {
+          ...state,
+          categories: [...state.categories, 'Under Construction'],
+        };
+      }
+      return {
+        ...state,
+        categories: state.categories,
+      };
+    },
+  },
+});
+
+export const { checkStatus } = categoriesSlice.actions;
+
+export default categoriesSlice.reducer;
